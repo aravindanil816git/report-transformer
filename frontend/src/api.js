34ec88b32d1889e441b5cb4a795412fd2ff882d0
Aravind A <aravindanil816@gmail.com>
2026-04-01 16:38:39 +0000

    del API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 
 import axios from "axios";
-const API = process.env.REACT_APP_API_URL || "http://localhost:8000";
+// const API = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API = process.env.REACT_APP_API_URL;
 
 export const listReports = () => axios.get(`${API}/reports`);
 export const createReport = (name, type, extra = {}) => {
